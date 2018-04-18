f3b1d71d5c52411cbdacaff7fb93a9b81bbde1b0
abe8301 <abe8301@gmail.com>
2018-04-18 21:22:13 +0200

    hamburger menu

---
+++ JS/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $('.menu-icon').click(function() {
+        $(this).toggleClass('active')
+    });
+
+    $('.menu-icon').click(function() {
+        $('.sidebar').toggleClass('active')
+    });
+});
